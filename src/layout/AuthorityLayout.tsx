f43e0be29a9cc9db2003a485c8936da1dfaffc7e
maomao1996 <1714487678@qq.com>
2020-05-17 20:57:19 +0800

    fix: 去除 AuthorityLayout 中的 Router

--- src/layout/AuthorityLayout.tsx
+++ src/layout/AuthorityLayout.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
 import { Layout } from 'antd'
 import Footer from './components/Footer'
 
@@ -12,16 +11,14 @@ interface AuthorityLayoutProps {}
 const AuthorityLayout: React.FC<AuthorityLayoutProps> = (props) => {
   const { children } = props
   return (
-    <Router>
-      <Layout className="m-authoritylayout">
-        <Sider />
-        <Layout>
-          <Header />
-          <Content>{children}</Content>
-          <Footer />
-        </Layout>
+    <Layout className="m-authoritylayout">
+      <Sider  />
+      <Layout>
+        <Header />
+        <Content>{children}</Content>
+        <Footer />
       </Layout>
-    </Router>
+    </Layout>
   )
 }
 
