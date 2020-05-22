9287badcbe36ee381a7846dc33fecd8a186c7619
maomao1996 <1714487678@qq.com>
2020-05-22 15:10:59 +0800

    feat: 添加 antd 全局化配置

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+import zhCN from 'antd/es/locale/zh_CN'
+import { ConfigProviderProps } from 'antd/es/config-provider'
+
+export const antdConfig: ConfigProviderProps = {
+  componentSize: 'middle',
+  locale: zhCN
+}
