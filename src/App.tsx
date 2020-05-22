7f36e77df64b1c71e9674af3b5b7b90d4e4973aa
maomao1996 <1714487678@qq.com>
2020-05-22 15:10:59 +0800

    feat: 添加 antd 全局化配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import React, { Suspense } from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { ConfigProvider } from 'antd'
 import RenderRouter from '@/routes/RenderRouter'
 import { routes } from '@/routes'
 import LoginContainer from '@/store/login'
+import { antdConfig } from './config'
 
 function App() {
   return (
     <LoginContainer.Provider>
-      <BrowserRouter>
-        <Suspense fallback={null}>
-          <RenderRouter routes={routes} />
-        </Suspense>
-      </BrowserRouter>
+      <ConfigProvider {...antdConfig}>
+        <BrowserRouter>
+          <Suspense fallback={null}>
+            <RenderRouter routes={routes} />
+          </Suspense>
+        </BrowserRouter>
+      </ConfigProvider>
     </LoginContainer.Provider>
   )
 }
