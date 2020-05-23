ab87893fb30adb3bf0eb4815aebeb650b874c8ca
maomao1996 <1714487678@qq.com>
2020-05-23 15:51:01 +0800

    feat: 固定侧边栏并支持收缩

--- src/layout/AuthorityLayout.tsx
+++ src/layout/AuthorityLayout.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Layout } from 'antd'
 import Header from './components/Header'
 import Sider from './components/AuthoritySider'
@@ -8,14 +8,23 @@ import './AuthorityLayout.scss'
 
 const { Content } = Layout
 
+const SiderWidth: number = 260
+
 interface AuthorityLayoutProps {}
 
 const AuthorityLayout: React.FC<AuthorityLayoutProps> = (props) => {
   const { children } = props
+
+  const [collapsed, setCollapsed] = useState<boolean>(false)
+
   return (
     <Layout className="m-authoritylayout">
-      <Sider />
-      <Layout>
+      <Sider
+        collapsed={collapsed}
+        onCollapse={setCollapsed}
+        width={SiderWidth}
+      />
+      <Layout style={{ paddingLeft: collapsed ? 80 : SiderWidth }}>
         <Header />
         <Content className="m-authoritylayout-content">{children}</Content>
         <Footer />
