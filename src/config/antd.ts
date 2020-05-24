0e609c5bafeb61e0610cf1854d09fe36ec804c5d
maomao1996 <1714487678@qq.com>
2020-05-24 11:02:18 +0800

    feat: 提取 layout 相关配置

--- src/config/antd.ts
+++ src/config/antd.ts
@@ -1,7 +1,14 @@
 import zhCN from 'antd/es/locale/zh_CN'
 import { ConfigProviderProps } from 'antd/es/config-provider'
 
+/**
+ * antd 全局配置
+ * https://ant.design/components/config-provider-cn/#API
+ */
+
 export const antdConfig: ConfigProviderProps = {
+  // 组件大小
   componentSize: 'middle',
+  // 语言包配置
   locale: zhCN
 }
