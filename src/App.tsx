5e5314a5ee5c3cb67f751276353272fb3222f6db
maomao1996 <1714487678@qq.com>
2020-07-20 20:38:44 +0800

    feat(renderrouter): 重写 `RenderRouter` 组件,使用嵌套路由配置 layout, 解决路由切换空白问题

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React, { Suspense } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { ConfigProvider } from 'antd'
-import RenderRouter from '@/routes/RenderRouter'
-import { routes, normalRoutes } from '@/routes'
+import RenderRouter from '@/components/RenderRouter'
+import { authorizedRoutes, normalRoutes } from '@/routes'
 import LoginContainer from '@/store/login'
 import { antdConfig } from './config'
 
@@ -13,7 +13,7 @@ const App: React.FC = () => {
     <ConfigProvider {...antdConfig}>
       <BrowserRouter>
         <Suspense fallback={null}>
-          <RenderRouter routes={isLogin ? routes : normalRoutes} />
+          <RenderRouter routes={isLogin ? authorizedRoutes : normalRoutes} />
         </Suspense>
       </BrowserRouter>
     </ConfigProvider>
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,47 +1,52 @@
 import { lazy } from 'react'
-import { RouteItem } from './RenderRouter'
-
-// 登录
-export const loginRoutes: RouteItem[] = [
-  {
-    path: '/login',
-    component: lazy(() => import('@/pages/login'))
-  }
-]
+import { RouteItem } from '@/components/RenderRouter'
 
 // 默认路由
 export const normalRoutes: RouteItem[] = [
-  ...loginRoutes,
   {
-    path: '*',
-    redirect: '/login'
+    path: '/',
+    component: lazy(() => import('@/layout/NormalLayout')),
+    routes: [
+      {
+        path: '/',
+        exact: true,
+        redirect: '/login'
+      },
+      {
+        path: '/login',
+        component: lazy(() => import('@/pages/login'))
+      },
+      {
+        path: '*',
+        redirect: '/login'
+      }
+    ]
   }
 ]
 
 // 权限路由
 export const authorizedRoutes: RouteItem[] = [
   {
-    layout: 'AuthorityLayout',
-    path: '/home',
-    component: lazy(() => import('@/pages/home'))
-  }
-]
-
-export const routes: RouteItem[] = [
-  ...authorizedRoutes,
-  ...loginRoutes,
-  {
-    layout: 'AuthorityLayout',
-    path: '/404',
-    component: lazy(() => import('@/pages/404'))
-  },
-  {
     path: '/',
-    exact: true,
-    redirect: '/home'
-  },
-  {
-    path: '*',
-    redirect: '/404'
+    component: lazy(() => import('@/layout/AuthorityLayout')),
+    routes: [
+      {
+        path: '/',
+        exact: true,
+        redirect: '/home'
+      },
+      {
+        path: '/home',
+        component: lazy(() => import('@/pages/home'))
+      },
+      {
+        path: '/404',
+        component: lazy(() => import('@/pages/404'))
+      },
+      {
+        path: '*',
+        redirect: '/404'
+      }
+    ]
   }
 ]
