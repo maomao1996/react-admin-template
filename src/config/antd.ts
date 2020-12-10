b403429fd8ae1df592f88faa87fba23341474a2d
maomao1996 <1714487678@qq.com>
2020-12-10 19:55:01 +0800

    fix(config): 修改 moment 语言配置

--- src/config/antd.ts
+++ src/config/antd.ts
@@ -1,6 +1,11 @@
 import zhCN from 'antd/es/locale/zh_CN'
 import { ConfigProviderProps } from 'antd/es/config-provider'
 
+import moment from 'moment'
+import 'moment/locale/zh-cn'
+
+moment.locale('zh-cn')
+
 /**
  * antd 全局配置
  * https://ant.design/components/config-provider-cn/#API
