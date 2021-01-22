711d8428c4a2287c263756d787383112444914ac
maomao1996 <1714487678@qq.com>
2021-01-22 16:51:10 +0800

    style(.eslintrc): 修改 eslint 规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,6 +41,7 @@ module.exports = {
     'react/button-has-type': 0,
     '@typescript-eslint/no-non-null-assertion': 0,
     '@typescript-eslint/no-explicit-any': 0,
-    '@typescript-eslint/no-unused-vars': [1, { argsIgnorePattern: '^_' }]
+    '@typescript-eslint/no-unused-vars': [1, { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-module-boundary-types': 0
   }
 }
